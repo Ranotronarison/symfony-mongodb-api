0273ca499f79870488b88d4a118b68d7f664cf13
ranotronarison <arivonyran@gmail.com>
2024-10-01 11:31:54 +0000

    feat: add functional tests for articles endpoints

--- tests/functional/ArticlesApiTest.php
+++ tests/functional/ArticlesApiTest.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 
-namespace App\Tests;
+namespace App\Functional\Tests;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use App\Document\Article;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
 
@@ -17,7 +18,8 @@ class ArticlesApiTest extends ApiTestCase
         $input = new ArrayInput([
           "command" => "doctrine:mongodb:fixtures:load",
           "--no-interaction" => true,
-          "--env" => "test"
+          "--env" => "test",
+          "--quiet" => true
         ]);
 
         $application->run($input);
@@ -34,6 +36,22 @@ class ArticlesApiTest extends ApiTestCase
         );
 
         return $response->toArray()["token"];
+    }
+
+    public function createArticle($name, $description, $price, $quantity): Article
+    {
+        $container = self::getContainer();
+        $documentManager = $container->get('doctrine_mongodb.odm.document_manager');
+
+        $article = new Article();
+        $article->setName($name);
+        $article->setDescription($description);
+        $article->setPrice($price);
+        $article->setQuantity($quantity);
+        $documentManager->persist($article);
+        $documentManager->flush();
+
+        return $article;
     }
 
     public function testGetArticleCollection()
@@ -67,5 +85,53 @@ class ArticlesApiTest extends ApiTestCase
         );
 
         $this->assertResponseStatusCodeSame(201);
+    }
+
+    public function testGetArticleItem()
+    {
+        $article = $this->createArticle("Article", "Article's description", 25.5, 5);
+        $token = $this->getToken();
+        static::createClient()->request(
+            'GET',
+            '/api/articles/'.$article->getId(),
+            [
+                "headers" => ["Authorization" => "Bearer ".$token]
+            ]
+        );
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testHardDeleteArticle()
+    {
+        $article = $this->createArticle("Article", "Article's description", 25.5, 5);
+        $token = $this->getToken();
+        static::createClient()->request(
+            'DELETE',
+            '/api/articles/'.$article->getId(),
+            [
+                "headers" => ["Authorization" => "Bearer ".$token]
+            ]
+        );
+        $this->assertResponseStatusCodeSame(204);
+    }
+
+    public function testUpdateArticle()
+    {
+        $article = $this->createArticle("Article", "Article's description", 25.5, 5);
+        $token = $this->getToken();
+        static::createClient()->request(
+            'PATCH',
+            '/api/articles/'.$article->getId(),
+            [
+                "headers" => [
+                    "content-type" => "application/merge-patch+json",
+                    "Authorization" => "Bearer ".$token
+                ],
+                "json" => [
+                    "name" => "Updated Article"
+                ]
+            ]
+        );
+        $this->assertResponseIsSuccessful();
     }
 }
